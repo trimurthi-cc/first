e3ac7be3de96e8582ee1eb13ee142ab668a90709
CareerIT <trimurthi.cc@gmail.com>
2022-02-24 06:35:45 +0530

    adding all files into repo

---
+++ src/main/java/com/careerit/group1/App.java
@@ -0,0 +1,13 @@
+package com.careerit.group1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
